e00d06db243fc726a1fe7e9dcef40d24acc218fc
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-08 19:24:43 +0100

    Create object songsProps

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function Heading({ title }) {
   return (<h1>{title}</h1>);
 };
 
-function SongPlayer({ showControls = true, audioURL }) {
+function SongPlayer({ showControls = true, songsProps }) {
+  const { audioURL, coverURL } = songsProps;
   return (
     <>
       <Heading title="Music Player" />
+      <img width="250px" height="250px" src={coverURL} alt="Song cover" />
       <audio controls={showControls}>
         <source src={audioURL}></source>
       </audio>
@@ -17,9 +19,18 @@ function SongPlayer({ showControls = true, audioURL }) {
 };
 
 function App() {
+  const currentSong = {
+    audioURL: "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3",
+    coverURL: "https://examples.devmastery.pl/assets/audio/deadfro5h.jpg",
+    title: "Deadfro5h",
+    artist: "starfrosh"
+  };
   return (
     <div className="App">
-      <SongPlayer showControls audioURL="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3" />
+      <SongPlayer
+        showControls
+        songsProps={currentSong}
+      />
     </div>
   );
 }
