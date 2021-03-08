d4b2031e41ce6e16065d73c43cf3eb1758f11809
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-08 14:57:55 +0100

    delete some files and create simple components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Heading() {
+  return (<h1>Music Player</h1>);
+};
+
+function SongPlayer() {
+  return (
+    <audio controls="true" >
+      <source src="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3"></source>
+    </audio>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <SongPlayer />
     </div>
   );
 }
