3ad25648d572a72ce8955f75c31c4d7322997c8f
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-09 09:35:51 +0100

    charge data from internet

--- src/App.js
+++ src/App.js
@@ -1,83 +1,81 @@
-
-import { useRef, useState } from 'react';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function Heading({ title }) {
-  return (<h1>{title}</h1>);
-};
+  return <h1>{title}</h1>;
+}
 
-function SongPlayer({ showControls = false, songsProps }) {
+function SongPlayer({ showControls = false, song }) {
   const audioRef = useRef();
-  const { audioURL, coverURL } = songsProps;
+  const { audioUrl, coverUrl } = song;
   return (
-    <>
+    <section>
       <Heading title="Music Player" />
-      <img width="250px" height="250px" src={coverURL} alt="Song cover" />
-      <audio ref={audioRef} key={audioURL} controls={showControls}>
-        <source src={audioURL}></source>
+      <img width="250" height="250" src={coverUrl} alt="Song cover" />
+      <audio ref={audioRef} key={audioUrl} controls={showControls}>
+        <source src={audioUrl} />
       </audio>
       <div>
         <button onClick={() => audioRef.current.play()}>Play</button>
         <button onClick={() => audioRef.current.pause()}>Pause</button>
       </div>
-    </>
+    </section>
   );
-};
-
+}
 function SongListItem({ song, isCurrent, onSelect }) {
-  const background = isCurrent ? "red" : "none";
+  const background = isCurrent ? "darkslategrey" : "none";
   const style = { background };
   function handleClick() {
     onSelect(song);
-  };
+  }
   return (
     <li style={style} onClick={handleClick}>
-    {song.title} by {song.artist}
-  </li>
+      {song.title} by {song.artist}
+    </li>
   );
-};
-
-function App() {
-  const songs = [
-    {
-      audioURL: "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3",
-      coverURL: "https://examples.devmastery.pl/assets/audio/deadfro5h.jpg",
-      title: "Deadfro5h",
-      artist: "starfrosh"
-    },
-    {
-      audioURL: "https://examples.devmastery.pl/assets/audio/majesty.mp3",
-      coverURL: "https://examples.devmastery.pl/assets/audio/majesty.jpg",
-      title: "Majesty (Original Mix)",
-      artist: "Ryan Craig Martin"
-    },
-    {
-      audioURL: "https://examples.devmastery.pl/assets/audio/runs.mp3",
-      coverURL: "https://examples.devmastery.pl/assets/audio/runs.jpg",
-      title: "Runs",
-      artist: "Wowa"
-    }
-  ];
-  const [currentSong, setCurrentSong] = useState(songs[1]);
+}
+export default function App() {
+  const URL = "https://examples.devmastery.pl/songs-api/songs";
+  const [songs, setSongs] = useState([]);
+  useEffect(() => {
+    fetch(URL).then((response) => {
+      if (response.ok) {
+        response.json().then(setSongs);
+      }
+    });
+  }, []);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const currentSong = songs[currentSongIndex];
   function handleSelectSong(selectedSong) {
-    setCurrentSong(selectedSong);
-  };
+    const audioIndex = songs.findIndex(
+      (song) => song.audioUrl === selectedSong.audioUrl
+    );
+    if (audioIndex >= 0) {
+      setCurrentSongIndex(audioIndex);
+    }
+  }
   return (
     <div className="App">
-      <SongPlayer songsProps={currentSong} />
-      <section>
-        <Heading title="Songs" />
-        <ul>
-          {songs.map((song) => <SongListItem
-            key={song.audioURL}
-            song={song}
-            isCurrent={currentSong.audioURL === song.audioURL }
-            onSelect={handleSelectSong}
-          />)}
-        </ul>
-      </section>
+      {songs.length === 0 ? (
+        "Loading..."
+      ) : (
+        <>
+          <SongPlayer song={currentSong} />
+          <section>
+            <Heading title="Songs" />
+            <ul>
+              {songs.map((song) => (
+                <SongListItem
+                  key={song.audioUrl}
+                  song={song}
+                  isCurrent={currentSong.audioUrl === song.audioUrl}
+                  onSelect={handleSelectSong}
+                />
+              ))}
+            </ul>
+          </section>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
